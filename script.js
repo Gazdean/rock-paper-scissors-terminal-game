ac718b4429bc725cedd56b1c062c68014154b912
Gareth Dean <gazdean@hotmail.com>
2025-11-13 12:34:09 +0000

    creates getHumanChoice function
    creates a function which prompts the user for their choice
    the prompt contains a useful message
    if the entered value is not in the choice array it reprompts the user
    the reprompt asks the user to check the spelling
    the orignal value the user entered is the default text in the box for the user to check

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 const log = console.log;
 
+const choiceArr = ['rock', 'paper', 'scissors'];
+
 function getComputerChoice() {
-    return Math.floor(Math.random() * 3) + 1
+    return Math.floor(Math.random() * 3) + 1;
+}
+
+function getHumanChoice(choiceArr) {
+    let humanChoice;
+    let message = "Please choose rock, paper, scissors";
+    let entryValue = '';
+
+    while (!humanChoice) {
+        entryValue = prompt(message, entryValue).toLowerCase().trim();
+        if (choiceArr.includes(entryValue)) humanChoice = entryValue;
+        else message += '\nPlease check the spelling'
+    }
+
+    return humanChoice
 }
